cbed77be41940e03d42305f02cd88f1a17d50072
root <root@localhost.localdomain>
2015-04-12 12:14:43 -0500

    Analysis Started

--- analysis.R
+++ analysis.R
@@ -1,2 +1,8 @@
 
-View(f_dataset)
+# Reading in weather and flight data weather data
+
+weather_table_24 = read.csv("../datasets/weather/weather_data_24.csv")
+f_dataset = read.csv("../datasets/SAT_flights.csv")
+
+View (f_dataset)
+View (weather_table_24)
