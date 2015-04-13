ddf36e87acb1b17b821fd563e892a110a6491b77
root <rahulunair@gmail.com>
2015-04-12 20:48:46 -0500

    Progressing with analysis - Max delay, Max avg delay, Min avg delay etc calculated

--- analysis.R
+++ analysis.R
@@ -1,5 +1,6 @@
 library(tidyr)
 library(dplyr)
+library(sqldf) # a very nice library to do sql like queries with data frames
 # Reading in weather and flight data weather data
 
 weather_table_24 = read.csv("../datasets/weather/weather_data_24.csv")
@@ -47,18 +48,75 @@ avg_delay <- f_data %>% group_by (date.hour) %>% filter( DepDel15 == 1)
     %>% filter ( count > 3 )
 avg_delay
 
+# max of average delay for more than 3 flights
+
+HighestAvgDelayDest <- sqldf("SELECT `date.hour`,Avg_DepDelayMinutes,count from avg_delay WHERE Avg_DepDelayMinutes =  (
+                SELECT max(Avg_DepDelayMinutes) FROM avg_delay);")
+
+ScndHighestAvgDelayDest <- sqldf("SELECT `date.hour`,Avg_DepDelayMinutes,count from avg_delay WHERE Avg_DepDelayMinutes =  (
+                SELECT max(Avg_DepDelayMinutes) FROM avg_delay
+                WHERE Avg_DepDelayMinutes NOT IN (SELECT max(Avg_DepDelayMinutes) FROM avg_delay));")
 
 
-# When was max delay
+
+# When was max delay for a date
 max_delay <- f_data %>% group_by (date.hour) %>% filter( DepDel15 == 1) %>% summarize (Max_DepDelayMinutes = max(DepDelayMinutes))
+
+names(max_delay)[names(max_delay) == "date.hour"] <- 'date_hour'
+
+
+
+# Departure delay table with date, max amount of delay in minutes for the date, carrier ID and destination city
+# Using this table, we can see when was the most delay and its details
+# when was the least delay and details
+# carrier with most number of delay
+# carrier with least number of delay
+# destination with most number of delay
+# destination with least number of delay
+
+max_delay_destination <- sqldf("select f_data.date_hour,Max_DepDelayMinutes,UniqueCarrier, DestCityName, Distance FROM max_delay 
+                                inner join f_data  on f_data.date_hour = max_delay.date_hour 
+                                and f_data. DepDelayMinutes = max_delay.Max_DepDelayMinutes
+                                order by max_delay.Max_DepDelayMinutes DESC;") 
+View(max_delay_destination)
+
+# finding the highest and second highest delay information
+HighestDelayDest <- sqldf("SELECT date_hour,Max_DepDelayMinutes,DestCityName,Distance from max_delay_destination WHERE Max_DepDelayMinutes =  (
+                SELECT max(Max_DepDelayMinutes) FROM max_delay_destination);")
+
+scndHighstDelayDest <- sqldf("SELECT date_hour,Max_DepDelayMinutes,DestCityName,Distance from max_delay_destination 
+                              WHERE Max_DepDelayMinutes =  (SELECT max(Max_DepDelayMinutes) FROM max_delay_destination 
+                              WHERE Max_DepDelayMinutes NOT IN (SELECT max(Max_DepDelayMinutes) FROM max_delay_destination));")
+
+
+scndHighstDelayDest <- sqldf("SELECT date_hour,Max_DepDelayMinutes,DestCityName,Distance from max_delay_destination 
+                              WHERE Max_DepDelayMinutes =  (SELECT max(Max_DepDelayMinutes) FROM max_delay_destination 
+                              WHERE Max_DepDelayMinutes NOT IN (SELECT max(Max_DepDelayMinutes) FROM max_delay_destination));")
+
+# when was the least delay
+
+
+LowestDelayDest <- sqldf("SELECT date_hour,Max_DepDelayMinutes,DestCityName,Distance from max_delay_destination WHERE Max_DepDelayMinutes =  (
+                SELECT min(Max_DepDelayMinutes) FROM max_delay_destination);")
+
+
+scndLowestDelayDest <- sqldf("SELECT date_hour,Max_DepDelayMinutes,DestCityName,Distance from max_delay_destination 
+                              WHERE Max_DepDelayMinutes =  (SELECT min(Max_DepDelayMinutes) FROM max_delay_destination 
+                              WHERE Max_DepDelayMinutes NOT IN (SELECT min(Max_DepDelayMinutes) FROM max_delay_destination));")
                                                                                        
-as.character(f_data[which.max(f_data[, "DepDelayMinutes"]), "DestCityName"])
-opt <- which.max(f_data[, "DepDelayMinutes"])
 
-index = which.max(max_delay$Max_DepDelayMinutes)
+# Departure delay table with date, avg amount of delay in minutes for the date, carrier ID and destination city
+# Using this table, we can see when was the most delay and its details
+# when was the least delay and details
+# carrier with most number of delay
+# carrier with least number of delay
+# destination with most number of delay
+# destination with least number of delay
 
-max_delay_date <- max_delay[index, 1]
-max_delay_date_delay <-  max_delay[index, 2]
+
+
+Destination1 =  labels(which.max(table(f_data$DestCityName)))
+table(f_data$DestCityName)
 
 
 
@@ -78,17 +136,17 @@ w_data_2 <- w_data %>% filter(Date!= "NA", Time!= "NA")
 w_data_2$hour = sapply(w_data_2$hour, function (x) if(x == "") x = "24" else x)
 
 # combining date and hour
-w_data_2 <- w_data_2 %>% unite("date.hour", Date, hour, sep = "-")
+w_data_2 <- w_data_2 %>% unite("date_hour", Date, hour, sep = "-")
 View(w_data_2)
 dim(w_data_2)
 
 #Selecting attibutes fom weather data
-w_data_3 <- w_data_2 %>% filter(date.hour != "NA", TemperatureF != "NA", Wind.SpeedMPH != "NA", VisibilityMPH != "NA")
+w_data_3 <- w_data_2 %>% filter(date_hour != "NA", TemperatureF != "NA", Wind.SpeedMPH != "NA", VisibilityMPH != "NA")
 dim(w_data_3)
 
 
 # Average weather - Averaging all values in a 24 hr period, getting max of weather conditions and events if any
-avg_weather <-  w_data_3 %>% group_by(date.hour) %>% summarise(Avg_Temperature_F = mean(TemperatureF),
+avg_weather <-  w_data_3 %>% group_by(date_hour) %>% summarise(Avg_Temperature_F = mean(TemperatureF),
                                                                Avg_wind_speed_MPH = mean (Wind.SpeedMPH),
                                                                Avg_Visibility = mean(VisibilityMPH),
                                                                conditions = labels(which.max(table(Conditions))),
@@ -96,7 +154,7 @@ avg_weather <-  w_data_3 %>% group_by(date.hour) %>% summarise(Avg_Temperature_F
                                                             
 
 dim (avg_weather)
-View(avg_weather)labels(which.max(table(w_data_3$Conditions)))
+
 
 # combined weather and flight data
 
